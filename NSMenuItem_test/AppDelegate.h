70acedb40caf06a8b3c1a814a8137e66173a4e5b
Gregory Casamento <greg.casamento@gmail.com>
2024-06-02 00:16:28 -0400

    Initial Commit

---
+++ NSMenuItem_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSMenuItem_test
+//
+//  Created by Gregory John Casamento on 6/2/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSMenuItem_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSMenuItem_test
+//
+//  Created by Gregory John Casamento on 6/2/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
